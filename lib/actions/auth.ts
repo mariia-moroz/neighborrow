404198b0bdb5fb24aa9e112cea28cd4ec726919a
mariia-moroz <mma.moroz.mariia@gmail.com>
2026-06-27 17:55:07 +0100

    Fixed email trigger

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -79,7 +79,7 @@ export const signUp = async (params: AuthCredentials) => {
     });
 
     await workflowClient.trigger({
-      url: `${config.env.prodApiEndpoint}/api/workflow/onboarding`,
+      url: `${config.env.prodApiEndpoint}/api/workflows/onboarding`,
       body: { email, fullName },
     });
 
